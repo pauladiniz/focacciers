aeabb4bc8dcd458e1c0ecfa63f45ec27828fde49
Paula Diniz <bomdiapaulaa@gmail.com>
2024-03-20 22:34:30 +0100

    feat: spotify update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="container">
   <div class="relative flex justify-center items-center">
-    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1V5ashEQG2LJju4FcG9daq?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5okMb8z9oICeTAmXdVmDOB?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     <div class="credits">
-      <span class="span-text">Developed by <a href="www.github.com/pauladiniz">Paula Diniz</a> with ❤️ @ 2024</span>
+      <span class="span-text">Developed by <a href="www.github.com/pauladiniz" target="_blank">Paula Diniz</a> with ❤️ @ 2024</span>
     </div>
   </div>
 
