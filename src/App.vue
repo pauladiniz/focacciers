10a8a6747d56cb03631170c186b97641bf3d5fd3
Paula Diniz <bomdiapaulaa@gmail.com>
2024-03-20 22:27:20 +0100

    feat: small details

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template>
-<div class="w-full">
-  <div class="flex justify-center items-center">
+<div class="container">
+  <div class="relative flex justify-center items-center">
     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1V5ashEQG2LJju4FcG9daq?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <div class="credits">
+      <span class="span-text">Developed by <a href="www.github.com/pauladiniz">Paula Diniz</a> with ❤️ @ 2024</span>
+    </div>
   </div>
+
 </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.credits {
+  position: absolute;
+  right: 80px;
+  bottom: 50px;
+}
+.span-text {
+  font-size: small;
+  color: #f6f6f6;
+}
+</style>
 
